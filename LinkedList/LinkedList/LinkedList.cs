6427c8968c8112679e078e430c640e6828eadc9c
nairitprachanda <nairitprachanda@gmail.com>
2020-11-28 15:24:07 +0530

    [nairit] refactor: deleting last node

--- LinkedList/LinkedList/LinkedList.cs
+++ LinkedList/LinkedList/LinkedList.cs
@@ -87,5 +87,19 @@ namespace LinkedList
             this.head = this.head.next;
             return this.head;
         }
+        internal Node PopLast()
+        {
+            if (head == null)
+                return null;
+            if (head.next == null)
+                return null;
+            Node newNode = head;
+            while (newNode.next.next != null)
+            {
+                newNode = newNode.next;
+            }
+            newNode.next = null;
+            return head;
+        }
     }
 }
--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -10,7 +10,7 @@ namespace LinkedList
             list.Add(56);
             list.Add(30);
             list.Add(70);
-            list.Pop();
+            list.PopLast();
             list.Display();
         }
     }
