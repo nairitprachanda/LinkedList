1eb4b8efd9d9a2e94d0b0aa09e2a71268e5eb614
nairitprachanda <nairitprachanda@gmail.com>
2020-11-28 15:13:12 +0530

    [nairit] refactor: appending nodes

--- LinkedList/LinkedList/LinkedList.cs
+++ LinkedList/LinkedList/LinkedList.cs
@@ -37,5 +37,20 @@ namespace LinkedList
                 temp = temp.next;
             }
         }
+        internal void Append(int new_data)
+        {
+            Node new_node = new Node(new_data);
+            if (head == null)
+            {
+                head = new Node(new_data);
+                return;
+            }
+            new_node.next = null;
+            Node last = head;
+            while (last.next != null)
+                last = last.next;
+            last.next = new_node;
+            return;
+        }
     }
 }
--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -7,9 +7,9 @@ namespace LinkedList
         {
             Console.WriteLine("Hello World!");
             LinkedList list = new LinkedList();
-            list.Add(70);
-            list.Add(30);
             list.Add(56);
+            list.Append(30);
+            list.Append(70);
             list.Display();
         }
     }
