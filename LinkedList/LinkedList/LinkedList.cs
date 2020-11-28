1713068c435755b47d43a5242433cfa3cd71f765
nairitprachanda <37877904+nairitprachanda@users.noreply.github.com>
2020-11-28 15:01:40 +0530

    Update LinkedList.cs

--- LinkedList/LinkedList/LinkedList.cs
+++ LinkedList/LinkedList/LinkedList.cs
@@ -1,10 +1,25 @@
-﻿using System;
+  
+using System;
 using System.Collections.Generic;
 using System.Text;
-
 namespace LinkedList
 {
-    class linkedlist
+    class LinkedList
     {
+        //internal Node head;
+        internal void Display(Node head)
+        {
+            Node temp = head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
     }
 }
